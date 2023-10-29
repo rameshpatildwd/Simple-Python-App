5c83a623049ab1a36d4e55a81546852592ea67af
rameshpatildwd <108170411+rameshpatildwd@users.noreply.github.com>
2023-10-29 11:18:20 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull docker push rameshpatildwd/simple-python-flask-app:latest
+docker pull rameshpatildwd/simple-python-flask-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 rameshpatildwd/simple-python-flask-app:latest
