606cd365e531ca8ed445f4657ce98309b1d6ff7f
rameshpatildwd <108170411+rameshpatildwd@users.noreply.github.com>
2024-02-02 05:02:03 +0000

    updated code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello Team Signaince, This is executed by Ramesh to complete the assesment!!'
+    return 'Hello Team, This is executed by Ramesh to complete the Second Assesment of AWS DevOps!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
